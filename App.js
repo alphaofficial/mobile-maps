06d41eceaa8d180205fbe16acbc8254a8f47836f
Albert Alpha <baronofficial23@gmail.com>
2019-08-01 02:08:06 +0000

    Check internet connection

--- App.js
+++ App.js
@@ -1,12 +1,85 @@
 import React from 'react';
-import { WebView } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-      <WebView
-        source={{uri: 'https://www.google.com/maps'}}
-        style={{marginTop: 20}}
-      />
-  );
+export default class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      isOnline: true,
+      connectionInfo: null,
+      details: ""
+    };
+
+  }
+
+
+  
+
+  componentDidMount(){
+    NetInfo.fetch().then(state => {
+      console.log("Connection type", state.type);
+      console.log("Is connected?", state.isConnected);
+      
+      this.setState({
+        isOnline: state.isConnected,
+        details: state.details
+      })
+
+    });
+
+    // Subscribe
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log("Connection type", state.type);
+      console.log("Is connected?", state.isConnected);
+      
+      this.setState({
+        isOnline: state.isConnected,
+        details: state.details
+      })
+
+    });
+
+    // Unsubscribe
+    //unsubscribe();
+   
+  }
+
+  render(){
+
+    if(this.state.isOnline === true){
+      return (
+          <WebView source={{ uri: 'https://www.google.com/maps' }} />
+      );
+    }
+    else{
+      return (
+        <React.Fragment>
+          <View style={styles.container}>
+            <Text style={styles.title}>
+              Check internet connection!
+            </Text>
+          </View>
+        </React.Fragment>
+      )
+    }
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   padding: 30
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'red'
+  },
+  activeTitle: {
+    color: 'red',
+  },
+});
 
